4eb8d14de3808d4a870d3115a16b451a1446b19c
kondoumh <kondoumh@gmail.com>
2020-03-03 13:42:19 +0900

    update usage comments

--- cmd/graph.go
+++ cmd/graph.go
@@ -9,13 +9,8 @@ import (
 // graphCmd represents the graph command
 var graphCmd = &cobra.Command{
 	Use:   "graph",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "generate graph structure",
+	Long: `generate graph structure of pages and authors (as dot file)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("graph called")
 	},
