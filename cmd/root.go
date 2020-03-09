9a4ce81e40e41ba7e7a5a8eb20e2e2d93d0a41ce
kondoumh <kondoumh@gmail.com>
2020-03-10 05:30:47 +0900

    override config value with flag

--- cmd/root.go
+++ cmd/root.go
@@ -2,13 +2,13 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
-
 
 // Config store settings
 type Config struct {
@@ -22,15 +22,12 @@ var config Config
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "sbv",
-	Short: "scrapbox-viz",
-	Long: `scrapbox-viz`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "A CLI to visualize Scrapbox project.",
+	Long:  `sbv is a CLI to analize and visualize Scrapbox project.`,
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -41,31 +38,22 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sbv.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("workdir", "d", "_work", "working directory")
+	viper.BindPFlag("workdir", rootCmd.PersistentFlags().Lookup("workdir"))
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".scrapbox-viz" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".sbv")
 	}
