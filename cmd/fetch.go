426ea3ec9e7f0d0e15811643b0c47ad02d783784
kondoumh <kondoumh@gmail.com>
2020-03-19 06:51:44 +0900

    fetch project page count

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/kondoumh/scrapbox-viz/pkg"
+	"github.com/kondoumh/scrapbox-viz/pkg/fetch"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +14,7 @@ import (
 var fetchCmd = &cobra.Command{
 	Use:   "fetch",
 	Short: "fetch all pages of the project",
-	Long: `fetch all pages of the project`,
+	Long:  `fetch all pages of the project`,
 	Run: func(cmd *cobra.Command, args []string) {
 		doFetch(cmd)
 	},
@@ -23,5 +27,24 @@ func init() {
 
 func doFetch(cmd *cobra.Command) {
 	project, _ := cmd.PersistentFlags().GetString("project")
-	fmt.Println("fetch all pages, project : ", project)
+	count, err := fetchCount(project)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("fetch all pages, %s : %d\n", project, count)
+}
+
+func fetchCount(projectName string) (int, error) {
+	url := fmt.Sprintf("%s/%s?limit=1", fetch.BaseURL, projectName)
+	data, err := fetch.FetchData(url)
+	if err != nil {
+		return -1, err
+	}
+	var project pkg.Project
+	err = json.Unmarshal(data, &project)
+	if err != nil {
+		return -1, err
+	}
+	return project.Count, nil
 }
