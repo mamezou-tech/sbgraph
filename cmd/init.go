ae5d0baa049f4b015a6fcf234221a3b0fc1b1911
kondoumh <kondoumh@gmail.com>
2020-03-30 14:07:50 +0900

    remove project flag from init command

--- cmd/init.go
+++ cmd/init.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/kondoumh/scrapbox-viz/pkg/api"
 	"github.com/kondoumh/scrapbox-viz/pkg/file"
 	"github.com/spf13/cobra"
 )
@@ -12,12 +11,12 @@ import (
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize working directory",
-	Long:  LongUsage(`
+	Long: LongUsage(`
 		Initalize working directory for Scrapbox project.
 
-		  sbv init -p <project name>
+		  sbv init
 		
-		if 'workdir' exists in $HOME/.sbv.yaml or set by -d(--workdir) flag, it will be created.
+		if 'workdir' exists in $HOME/.sbv.yaml or set by global -d(--workdir) flag, it will be created.
 		`),
 	Run: func(cmd *cobra.Command, args []string) {
 		doInit(cmd)
@@ -25,27 +24,11 @@ var initCmd = &cobra.Command{
 }
 
 func init() {
-	initCmd.PersistentFlags().StringP("project", "p", "help-jp", "Name of Scrapbox project (required)")
 	rootCmd.AddCommand(initCmd)
 }
 
 func doInit(cmd *cobra.Command) {
-	project, _ := cmd.PersistentFlags().GetString("project")
-	fmt.Println("init called, project : ", project)
+	fmt.Printf("Check and create workdir : %s", config.WorkDir)
 	err := file.CreateDir(config.WorkDir)
 	CheckErr(err)
-	err = fetchProject(project)
-	CheckErr(err)
-}
-
-func fetchProject(project string) error {
-	data, err := api.FetchIndex(project)
-	if err != nil {
-		return err
-	}
-
-	if err := file.WriteBytes(data, project+".json", config.WorkDir); err != nil {
-		return err
-	}
-	return nil
 }
